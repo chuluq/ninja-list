9d51f1c8bd9410bfcfb55fe994d8caf4af60e473
chuluq <49191833+chuluq@users.noreply.github.com>
2021-03-03 11:11:48 +0700

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,29 @@
+import Head from 'next/head';
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name='keywords' content='ninjas' />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit, eius
+          corporis repudiandae beatae id molestiae placeat aspernatur in quo
+          deserunt doloribus esse voluptatem dolores, debitis culpa aperiam
+          itaque. Eius, qui.
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit, eius
+          corporis repudiandae beatae id molestiae placeat aspernatur in quo
+          deserunt doloribus esse voluptatem dolores, debitis culpa aperiam
+          itaque. Eius, qui.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
